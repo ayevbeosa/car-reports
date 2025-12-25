b9458e32922c7c587c0ca6742121fab933f9108a
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2025-12-25 20:34:38 +0100

    testing report approval

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { Repository } from 'typeorm';
@@ -14,6 +14,19 @@ export class ReportsService {
   create(reportDto: CreateReportDto, user: User) {
     const report = this.repository.create(reportDto);
     report.user = user;
+    return this.repository.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repository.findOne({
+      where: { id: parseInt(id) },
+    });
+
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
     return this.repository.save(report);
   }
 }
