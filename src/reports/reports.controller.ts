a2d19fee6bb45c6d3634ad5d38ee37a9593c8702
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2025-12-25 16:26:16 +0100

    Setting up association

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,15 +2,16 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorators';
+import { User } from '../users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
-
   constructor(private reportsService: ReportsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
