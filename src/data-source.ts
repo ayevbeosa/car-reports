2b5254f83e1d7d5b96a27fc05e6740f8f9a9afae
Ayevbeosa Iyamu <ayevbeosa.j@gmail.com>
2025-12-26 18:06:33 +0100

    Production DB config

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,37 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 
-const dbConfigs: Record<string, string> = {
-  development: 'db.sqlite',
-  test: 'test.sqlite',
-  production: '',
+const dbConfigs: Record<string, Partial<DataSourceOptions>> = {
+  development: {
+    type: 'sqlite',
+    database: 'db.sqlite',
+    logging: true,
+  },
+  test: {
+    type: 'sqlite',
+    database: 'test.sqlite',
+    logging: false,
+  },
+  production: {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: ['error'],
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 const env = process.env.NODE_ENV || 'development';
-const database = dbConfigs[env];
+const currentConfig = dbConfigs[env];
 
-if (!database) {
+if (!currentConfig) {
   throw new Error(`No database configuration found for environment: ${env}`);
 }
 
 export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: database,
+  ...currentConfig,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   migrationsRun: true,
   synchronize: false,
-});
+} as DataSourceOptions);
